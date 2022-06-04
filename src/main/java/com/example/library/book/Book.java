a0923aa6a5c29117f1a3b94efea98f8678891e14
Robin Rawat <robin.rawat@hashstudioz.com>
2022-06-04 12:02:00 +0530

    Initial commit

---
+++ src/main/java/com/example/library/book/Book.java
@@ -0,0 +1,18 @@
+package com.example.library.book;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+}
