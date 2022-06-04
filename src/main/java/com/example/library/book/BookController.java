3da66ef897d955fefa0e9a967a7a7cfb48e11b66
Robin Rawat <robin.rawat@hashstudioz.com>
2022-06-04 17:19:08 +0530

    add pagination and sorting

--- src/main/java/com/example/library/book/BookController.java
+++ src/main/java/com/example/library/book/BookController.java
@@ -1,11 +1,13 @@
 package com.example.library.book;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
@@ -15,7 +17,22 @@ public class BookController {
     BookRepository bookRepository;
 
     @GetMapping
-    List<Book> getBooks() {
-        return bookRepository.findAll();
+    Page<Book> getBooks(
+            @RequestParam Optional<Integer> page,
+            @RequestParam Optional<String> sortBy
+    ) {
+        return bookRepository.findAll(
+                PageRequest.of(
+                        page.orElse(0),
+                        5,
+                        Sort.Direction.ASC,
+                        sortBy.orElse("id")
+                )
+        );
+    }
+
+    @GetMapping("/{id}")
+    Optional<Book> getBook(@PathVariable Long id) {
+        return bookRepository.findById(id);
     }
 }
